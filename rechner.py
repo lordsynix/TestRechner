cf9e24fc0f62d1a34677d41a74983382a3c7fa8c
luisschifferlis <schifferliluis@gmail.com>
2023-08-31 15:48:18 +0200

    rechner

--- rechner.py
+++ rechner.py
@@ -3,5 +3,7 @@ import subtract
 import divide
 
 print(addieren.add(1, 2))
-print(subtract.sub(2, 1))
-print(int(divide.div(5, 5)))
+print(subtract.subtract(2, 1))
+print(int(divide.divide(5, 5)))
+
+print((addieren.add(10,15)))
